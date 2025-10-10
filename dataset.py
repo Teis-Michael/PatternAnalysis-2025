d184f1a2b91176404cbca798140ada44da68afac
claire_moldavite <teismichael0@gmail.com>
2025-10-10 14:19:18 +1000

    OASIS dataset from rangpur, opening files from dataset with python in dataset.py

---
+++ dataset.py
@@ -0,0 +1,49 @@
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import numpy as np
+import torch
+import torchvision
+from torch import nn
+from torch import optim
+import PIL
+import os
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+#dataget preproccessed from rangpur
+#downloaded local 
+
+#get 10 pic from directory
+number_png = 10
+count = 0
+test_set = []
+directory = r"C:\Users\teism\PatternAnalysis-2025-1\keras_png_slices_data"
+for entry in os.scandir(directory):  
+    if entry.is_file():  
+        print(entry.path)
+        a = PIL.Image.open(entry.path)
+        test_set.append(a)
+        count+=1
+    if count >= number_png:
+        break
+
+#uncomment to view png
+#for i in test_set:
+#    i.show()
+
+#class to store the picture and convert into a usable numpy array
+class ImageDataset(torch.utils.data.Dataset):
+    def __init__(self, X):
+        print(type(X))
+        self.X = X
+    def __len__(self):
+        return len(self.X)
+    def __getitem__(self, idx):
+        #TODO determine correct label value
+        return np.array(self.X[idx]), idx
+
+train_customdataset = ImageDataset(test_set)
+batch_size = 3
+#train_loader = torch.utils.data.DataLoader(dataset=train_customdataset, batch_size=batch_size, shuffle=True)
+
+#test loader
---
+++ modules.py
@@ -0,0 +1,6 @@
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import numpy as np
+import torch
